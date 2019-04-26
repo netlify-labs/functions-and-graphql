2b87d2f944b0b65851c8f90e11f8ee462923b575
DavidWells <hello@davidwells.io>
2019-04-26 15:37:51 -0700

    func update

--- functions/graphql.js
+++ functions/graphql.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
-      return 'Hello, world!'
+      return `I'm running in a Netlify function. https://bit.ly/2UXh0fD`
     }
   }
 }
